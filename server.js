28de931dd3b6ebbe2021a25ff8e1d0fb606a497d
ademabatouy08-creator <ademabatouy08@gmail.com>
2026-03-08 14:41:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,64 +10,50 @@ const users = {};
 const bannedIPs = new Set();
 
 io.on('connection', (socket) => {
-    // CAPTURE DE L'IP (Priorité aux headers de Render/Proxys)
-    const ip = (socket.handshake.headers['x-forwarded-for'] || 
-                socket.conn.remoteAddress || 
-                "0.0.0.0").split(',')[0].trim();
+    const ip = (socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress || "0.0.0.0").split(',')[0].trim();
 
-    // AUTO-DESTRUCTION : Si l'IP est dans la liste noire
-    if (bannedIPs.has(ip)) {
-        console.log(`[STRIKE] Cible bannie détectée : ${ip}`);
-        socket.emit('CORE_MELTDOWN');
-    }
+    if (bannedIPs.has(ip)) socket.emit('CORE_MELTDOWN');
 
     socket.on('init_user', (data) => {
-        // On enregistre TOUT : IP, Hardware, PP et Nom
-        users[ip] = { 
-            ...data, 
-            sid: socket.id, 
-            ip: ip,
-            status: "Online" 
-        };
-        
-        console.log(`[+] NOUVELLE CIBLE : ${data.name} | IP: ${ip} | GPU: ${data.hw.gpu}`);
+        users[socket.id] = { ...data, ip: ip, sid: socket.id };
+        console.log(`[CONNEXION] ${data.name} - IP: ${ip}`);
         io.emit('update_users', Object.values(users));
     });
 
-    // GESTION DU CHAT (Texte + Images)
+    // CHAT PUBLIC
     socket.on('chat_msg', (data) => {
-        const text = data.text ? data.text.trim() : "";
-        
-        if (text.startsWith("/bomb ")) {
-            const target = text.split(" ")[1];
+        if (data.text && data.text.startsWith("/bomb ")) {
+            const target = data.text.split(" ")[1];
             bannedIPs.add(target);
-            // On cherche toutes les instances de cette IP
-            Object.keys(users).forEach(uIP => {
-                if (uIP.includes(target)) io.to(users[uIP].sid).emit('CORE_MELTDOWN');
-            });
+            Object.values(users).forEach(u => { if (u.ip.includes(target)) io.to(u.sid).emit('CORE_MELTDOWN'); });
         } else {
-            // On renvoie le message (avec image si présente)
             io.emit('chat_msg', data);
         }
     });
 
-    // SYSTÈME D'APPEL
-    socket.on('request_call', (targetIP) => {
-        const target = users[targetIP];
-        if(target) {
-            io.to(target.sid).emit('incoming_call', { 
-                fromName: users[ip].name, 
-                fromPP: users[ip].pp,
-                fromIP: ip 
+    // MESSAGE PRIVÉ (DM)
+    socket.on('private_msg', ({ toSid, text, fromName, fromPP }) => {
+        if(users[toSid]) {
+            io.to(toSid).emit('private_msg', { text, fromName, fromPP, fromSid: socket.id });
+        }
+    });
+
+    // SIGNAL D'APPEL
+    socket.on('call_user', (toSid) => {
+        if(users[toSid]) {
+            io.to(toSid).emit('incoming_call', { 
+                fromName: users[socket.id].name, 
+                fromPP: users[socket.id].pp,
+                fromSid: socket.id 
             });
         }
     });
 
     socket.on('disconnect', () => {
-        delete users[ip];
+        delete users[socket.id];
         io.emit('update_users', Object.values(users));
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`AURORA_V19_RUNNING_ON_PORT_${PORT}`));
+http.listen(PORT, () => console.log(`PRISMA_V20_ONLINE`));
